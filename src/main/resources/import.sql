f250d4a5818e34c1d74ef00d076bc0b9365bcbfb
eduardo <lalorhp@gmail.com>
2020-04-27 01:18:22 -0500

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO producto(nombre,precio,create_at) VALUES ('Panasonic', 800, NOW());
+INSERT INTO producto(nombre,precio,create_at) VALUES ('Sony', 650, NOW());
+INSERT INTO producto(nombre,precio,create_at) VALUES ('Apple', 653, NOW());
+INSERT INTO producto(nombre,precio,create_at) VALUES ('Hawlett', 254, NOW());
+INSERT INTO producto(nombre,precio,create_at) VALUES ('Bianchi', 225, NOW());
+INSERT INTO producto(nombre,precio,create_at) VALUES ('Nike', 458, NOW());
+INSERT INTO producto(nombre,precio,create_at) VALUES ('Adidas', 452, NOW());
+INSERT INTO producto(nombre,precio,create_at) VALUES ('Reenbok', 145, NOW());
